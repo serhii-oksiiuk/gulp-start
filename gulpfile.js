cd345bffe0bad692c26e85edee958d9f4cdd585d
Serhii Oksiiuk <oksiiuk.serhii@gmail.com>
2021-09-29 12:32:42 +0300

    html email

--- gulpfile.js
+++ gulpfile.js
@@ -38,7 +38,7 @@ function images(){
 
 function scripts(){
     return src([
-                'node_modules/jquery/dist/jquery.js',
+                'app/js/jquery.js',
                 'app/js/main.js'
             ])
             .pipe(concat('main.min.js'))
